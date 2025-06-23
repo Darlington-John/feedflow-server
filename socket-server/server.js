a6a483b1d9765576a280b35eec4df4697758a65d
John Darlington <onuohadarlington38@gmail.com>
2025-06-23 08:11:20 +0000

    testing

---
+++ socket-server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("sendMessage", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("newMessage", msg); // broadcast to everyone
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Socket server running on port ${PORT}`));
