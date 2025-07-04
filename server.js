06d5f1f722086a08e1f9c020390941cfd1df1945
Darlington-John <onuohadarlington38@gmail.com>
2025-07-04 21:59:47 +0100

    new cooommmit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://feedflow.vercel.app",
+    origin: "https://feedflow-gamma.vercel.app",
     methods: ["GET", "POST"],
   },
 });
